715ddf4d6ed77496eac81765a06a9e8a09767bcd
TichaTin <meiliizzsuju@gmail.com>
2023-02-17 00:57:31 +1100

    get book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,12 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_author = Object.values(books).filter((book) => book.author === author);
+  if (filtered_author.length == 0) {
+    return res.status(204).json({message: "Can't be found"});
+  }
+  res.send(filtered_author)
 });
 
 // Get all books based on title
