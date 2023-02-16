3ca025fbd0f37d363a3d0fdaa5599817959a2617
TichaTin <meiliizzsuju@gmail.com>
2023-02-17 01:09:16 +1100

    Get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,8 +43,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let get_book = books[isbn];
+  let book_reviews = get_book['reviews']
+  if (Object.keys(book_reviews).length == 0) {
+    return res.status(204).json({message: "Can't be found"});
+  }
+  res.send(book_reviews)
 });
 
 module.exports.general = public_users;
