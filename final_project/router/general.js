321563510c7441ccc341b0948ac86b21e7177098
TichaTin <meiliizzsuju@gmail.com>
2023-02-17 00:59:39 +1100

    get book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,8 +33,12 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filtered_title = Object.values(books).filter((book) => book.title === title);
+  if (filtered_title.length == 0) {
+    return res.status(204).json({message: "Can't be found"});
+  }
+  res.send(filtered_title)
 });
 
 //  Get book review
